83587c9c8513fbc182e24fb35e9a1c77d1098336
bibilchacko151@gmail.com <bibilchacko151@gmail.com>
2025-07-12 16:50:08 +0530

    introduction to langchain

--- ice_breaker.py
+++ ice_breaker.py
@@ -1,9 +1,9 @@
 from dotenv import load_dotenv
 from azure.identity import DefaultAzureCredential, get_bearer_token_provider
 from langchain_openai import AzureChatOpenAI
+from langchain_ollama import ChatOllama
 from langchain_core.prompts import PromptTemplate
-
-
+from langchain_core.output_parsers import StrOutputParser
 
 import os
 if __name__ == "__main__":
@@ -22,6 +22,8 @@ if __name__ == "__main__":
         azure_endpoint=os.getenv("ENDPOINT_URL"),
         api_version="2025-01-01-preview"
         )
+    #ollama_llm = ChatOllama(model="gemma3")
+    ollama_mistral = ChatOllama(model="mistral")
     summary_template="""
     given the information {information} about a person i want you to create:
     1. a short summary
@@ -37,9 +39,11 @@ if __name__ == "__main__":
     In 2002, Musk founded the space technology company SpaceX, becoming its CEO and chief engineer; the company has since led innovations in reusable rockets and commercial spaceflight. Musk joined the automaker Tesla as an early investor in 2004 and became its CEO and product architect in 2008; it has since become a leader in electric vehicles. In 2015, he co-founded OpenAI to advance artificial intelligence (AI) research but later left; growing discontent with the organization's direction and their leadership in the AI boom in the 2020s led him to establish xAI. In 2022, he acquired the social network Twitter, implementing significant changes and rebranding it as X in 2023. His other businesses include the neurotechnology company Neuralink, which he co-founded in 2016, and the tunneling company the Boring Company, which he founded in 2017.
     Musk was the largest donor in the 2024 U.S. presidential election, and is a supporter of global far-right figures, causes, and political parties. In early 2025, he served as senior advisor to United States president Donald Trump and as the de facto head of DOGE. After a public feud with Trump, Musk left the Trump administration and announced he was creating his own political party, the America Party.
     Musk's political activities, views, and statements have made him a polarizing figure, especially following the COVID-19 pandemic. He has been criticized for making unscientific and misleading statements, including COVID-19 misinformation and promoting conspiracy theories, and affirming antisemitic, racist, and transphobic comments. His acquisition of Twitter was controversial due to a subsequent increase in hate speech and the spread of misinformation on the service. His role in the second Trump administration attracted public backlash, particularly in response to DOGE.
-
     """
-    chain = summary_promp_template | llm
+    # chain = summary_promp_template | llm
+    # res = chain.invoke(input={"information":information})
+    #chain = summary_promp_template | ollama_llm| StrOutputParser()
+    chain = summary_promp_template | ollama_mistral| StrOutputParser()
     res = chain.invoke(input={"information":information})
     # messages = [(
     #     "system",

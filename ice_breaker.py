124ceadc288b25e9e7aed52e53b531bd03fe2c61
bibilchacko151@gmail.com <bibilchacko151@gmail.com>
2025-07-12 15:37:33 +0530

    third commit

--- ice_breaker.py
+++ ice_breaker.py
@@ -1,5 +1,30 @@
-import os
+from dotenv import load_dotenv
+from azure.identity import DefaultAzureCredential, get_bearer_token_provider
+from langchain_openai import AzureChatOpenAI
 
+
+
+import os
 if __name__ == "__main__":
+    load_dotenv()
     print("hello world")
-    print(os.environ["OPEN_API_KEY"])
+    credentials = DefaultAzureCredential()
+    azure_ad_token_provider_bearer = get_bearer_token_provider(
+    credentials,
+    "https://cognitiveservices.azure.com/.default")
+    llm = AzureChatOpenAI(
+        azure_ad_token_provider=azure_ad_token_provider_bearer,
+        temperature=0.7,
+        max_tokens=256,
+        azure_deployment=os.getenv("DEPLOYMENT_NAME"),
+        azure_endpoint=os.getenv("ENDPOINT_URL"),
+        api_version="2025-01-01-preview"
+        )
+    messages = [(
+        "system",
+        "You are a helpful assistant that translates English to French. Translate the user sentence.",),("human", "I love programming."),]
+    ai_msg = llm.invoke(messages)
+    print(ai_msg)
+
+    
+    
